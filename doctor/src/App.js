74027a154f131d5780edcb69992b68ce37d65916
pcg15 <pcg15@duke.edu>
2018-04-03 14:23:02 -0400

    add app code without functioning output

--- doctor/src/App.js
+++ doctor/src/App.js
@@ -1,18 +1,100 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import AppBar from 'material-ui/AppBar';
+import Toolbar from 'material-ui/Toolbar';
+import Typography from 'material-ui/Typography';
+import Paper from 'material-ui/Paper';
+import TextField from 'material-ui/TextField';
+import Button from 'material-ui/Button';
+import axios from 'axios';
 
-class App extends Component {
+
+var styles = {
+    "appBarStyle": {
+            "marginBottom": "10px",
+            "backgroundColor": "#FC0909",
+        },
+    "paperStyle": {
+            "height": "1000px",
+            "width": "1000px",
+            "marginLeft": "200px",
+            "marginTop": "30px",
+            "textAlign": "center",
+            "display": "inline-block",
+            "padding": "10px",
+        },
+    "textFieldStyle": {
+            "marginTop": "30px",
+        },
+    "buttonStyle": {
+            "backgroundColor": "#FC0909",
+            "marginTop": "30px",
+            "color": "white",
+        }
+}
+
+
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            "patientEmail": "",
+            "outputTable": [null]
+        }
+    }
+
+    onTextFieldChange = (event) => {
+        this.setState({"patientEmail": event.target.value});
+    }
+
+    getData = () => {
+        var url_str = "http://0.0.0.0:5000/api/heart_rate/get_data/"
+        var txtField_str = this.state.patientEmail
+        var url_full = url_str.concat(txtField_str)
+        axios.get(url_full).then( (response) => {
+            console.log(response);
+            const allData = [];
+            for (let i=0; i < response.data.time_stamp.length; i++) {
+                allData.push({
+                    "times": response.data.time_stamp[i],
+                    "rates": response.data.user_heart_rate[i]
+                });
+            }
+            console.log(allData)
+            this.setState({"ouputTable": allData});
+            console.log(this.state.outputTable)
+        })
+    }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <AppBar position="static" style={styles.appBarStyle}>
+            <Toolbar>
+                <Typography variant="title" color="inherit">
+                    Doctor Viewer
+                </Typography>
+            </Toolbar>
+        </AppBar>
+        <Paper position="static" style={styles.paperStyle} zDepth={50}>
+            <div>
+            Welcome Doctor.
+            </div>
+            <div>
+            Please input patient email below.
+            </div>
+            <TextField style={styles.textFieldStyle}
+                value={this.state.txtEmail}
+                onChange={this.onTextFieldChange}/>
+            <div>
+            <Button variant="raised" style={styles.buttonStyle}
+                onClick={this.getData}>
+                Search
+            </Button>
+            <div>
+                {this.state.outputTable}
+            </div>
+            </div>
+        </Paper>
       </div>
     );
   }
